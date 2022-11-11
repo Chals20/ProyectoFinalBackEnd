5120ee4deed63c072e4add06b246a3e8f0a50e93
Chals <csanchezbasket@gmail.com>
2022-11-11 09:11:22 +0100

    Commit inicial BackEnd Proyecto final

---
+++ ProyectoFinalBackEnd/src/main/java/com/BackEnd/ProyectoFinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalBackEndApplication.class, args);
+	}
+
+}
